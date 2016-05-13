ff94858f9f6b40135dce4c9cc07b51db76652eb7
Ocean <Ocean@Ocean-PC>
2016-05-13 22:48:22 +0600

    new commit

---
+++ src/factorypattern/RocketEnemyShip.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorypattern;
+
+/**
+ *
+ * @author Ocean
+ */public class RocketEnemyShip extends EnemyShip {
+	     
+	    public RocketEnemyShip(){
+	         
+	        setName("Rocket Enemy Ship");
+	         
+	        setDamage(10.0);
+	         
+	    }
+
+    
+}
